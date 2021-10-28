2c2c30d77b78841bd0c6d48474d1de7e4f431a4d
Ahmad Syamil <57403084+madlim24@users.noreply.github.com>
2021-10-28 13:47:25 +0800

    Update FizzBuzz.java

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -3,7 +3,8 @@ package com.techprimers.testing;
 public class FizzBuzz {
 
     public String play(int number) {
-
+        // testing 
+        
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
